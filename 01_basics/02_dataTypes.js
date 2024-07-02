34344688c41ea5e3aec1238edc980fd6a96ba91d
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2024-07-02 16:21:34 +0000

    DataTypes Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all the JS code as a newer version
+
+//alert(3+3)  //Not accurate,Error -->> We are using Node JS, not browser
+
+let name ="Subhadip"
+let age = 19
+let isLoggedIn = true
+let temperature = null
+let city;
+
+//Number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//undefined => Its mean value isn't assigned
+//Null => Its a data type but also a value, but not undefined. Standalone value
+//symbols => Unique
+
+//Object
+
+console.log(typeof undefined); // Output -> undefined
+console.log(typeof null); // Output -> object
